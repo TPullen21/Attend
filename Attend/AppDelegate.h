cb39c33b9d1e19f5defde1f4dd89670dd3cb1f6a
Tom Pullen <tpullen21@gmail.com>
2015-11-26 16:14:13 +0000

    Initial Commit

---
+++ Attend/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Attend
+//
+//  Created by Tom Pullen on 26/11/2015.
+//  Copyright © 2015 Tom Pullen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Attend/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Attend
+//
+//  Created by Tom Pullen on 26/11/2015.
+//  Copyright © 2015 Tom Pullen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

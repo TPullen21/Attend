79fe6d69ccfedc1b7bcd7a4377f9103c08a2a36e
Tom Pullen <tpullen21@gmail.com>
2016-04-27 18:28:57 +0100

    Minor tweaks

--- Attend/ViewAttendanceViewController.m
+++ Attend/ViewAttendanceViewController.m
@@ -25,7 +25,7 @@
     
     NSURL *url = [[NSURL alloc] initWithString:urlString];
     NSURLRequest *request = [[NSURLRequest alloc] initWithURL:url];
-    //self.webView.scalesPageToFit = YES;
+    
     [self.webView loadRequest:request];
 }
 
@@ -33,16 +33,6 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 - (IBAction)closeButtonPressed:(id)sender {
     [self dismissViewControllerAnimated:YES completion:nil];
